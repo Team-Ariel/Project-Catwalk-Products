7e95dbe7832552173d73f5352ff2b50b870cb6ee
James Plier <plier2j@gmail.com>
2021-11-29 15:35:52 -0500

    Add handler to new branch

---
+++ handler.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+const servers = [
+  'http://13.59.91.177:3000',
+  'http://18.191.185.80:3000'
+]
+
+let currentServer = 0;
+
+
+app.get('*', (req, res) => {
+  let params = {
+    url: `${servers[currentServer]}${req.url}`,
+    method: 'get',
+    headers: req.headers,
+    data: req.body,
+  }
+  if (currentServer === 1) {
+    axios(params)
+    .then((results) => {
+      currentServer = 0;
+      res.status(200).send(results.data);
+    })
+    .catch((err) => res.status(404).send(err))
+  } else {
+    axios(params)
+    .then((results) => {
+      currentServer = 1;
+      res.status(200).send(results.data);
+    })
+    .catch((err) => res.status(404).send(err))
+  }
+
+
+})
+
+app.listen(3000, err => {
+  err ?
+  console.log("Failed to connect on port 3000") :
+  console.log("listening on port 3000")
+})
